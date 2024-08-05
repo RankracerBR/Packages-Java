8f4caa37251da6b0e8c362dbfcc49ca31021928a
Augusto Pontes <augustopontes010@gmail.com>
2024-08-05 10:37:44 -0300

    Java Packages/OOP

--- src/MSNMessenger.java
+++ src/MSNMessenger.java
@@ -1,18 +1,3 @@
-public class MSNMessenger {
-    public void enviarMensagem(){
-        System.out.println("Enviando Mensagem");
-    }
-
-    public void receberMensagem(){
-        System.out.println("Recebendo Mensagem");
-    }
-
-    public void validarConectadoInternet(){
-        System.out.println("Validando se está conectado a internet");
-    }
-
-    public void salvarHistoricoMensagem(){
-        System.out.println("Salvando o histórico da mensagem");
-    }
+public class MSNMessenger extends ServicoMensagemInstantanea{
 
 }
